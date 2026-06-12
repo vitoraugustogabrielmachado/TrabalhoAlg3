4f6b9a4a632af3fb02d5fc893f9eb0743604c7ae
HugoVictorTs <134705839+HugoVictorTs@users.noreply.github.com>
2026-06-12 19:35:22 -0300

    Update inicio.c

--- inicio.c
+++ inicio.c
@@ -4,7 +4,14 @@
 #include "arvore.h"
 #include "heap.h"
 #include "util.h"
+#define INF INT_MAX
 
+struct ponto{
+    double prio;
+    int index; // indice do ponto no vetor de pontos (coordenada x)
+    int viz_esq;
+    int viz_dir;
+};
 
 int main(int argc, char *argv[]){
     if(argv[1][1] != 'h' && argv[1][1] != 'a'){
@@ -12,39 +19,77 @@ int main(int argc, char *argv[]){
         return(0);
     }
 
-    long int numero;
-    scanf("%ld", &numero); //numero de elementos
+    long int num;
+    scanf("%ld", &num); //numero de elementos
 
-    double *vetor = inicializaVetor(numero); //o vetor com os pontos 
-    double *vetorheap = inicializaVetor(numero); //o vetor com as distancias calculadas
+    double vet_pontos[num+1]; //o vetor com os pontos (comeca no indice 1 e vai ate num)
+    struct ponto heap[num+1]; //o heap (vetor de structs)
+    int ts[num+1]; // a tabela de simbolos
 
-    for(int i = 1; i <= numero ; i++)
-        scanf("%lf", &vetor[i]); //leitura dos pontos
+    for(int i = 1; i <= num ; i++){
+        scanf("%lf", &vet_pontos[i]); // leitura dos pontos
+        heap[i].index = i; // inicia campo index do heap
+        // inicia campo vizinhos do heap
+        if (i == 1){
+            heap[i].viz_esq = -1;
+            heap[i].viz_dir = i + 1;
+        }
+        else if (i == num){
+            heap[i].viz_esq = i - 1;
+            heap[i].viz_dir = -1;
+        }
+        else{
+            heap[i].viz_esq = i - 1;
+            heap[i].viz_dir = i + 1;
+        }
+        ts[i] = i; // inicia ts
+    }
 
-    double distancia;
+    double prioridade;
     int tamHeap = 0;
 
     //para todas as funcoes: OS DOIS VETORES COMEÇAM EM 1 E NAO EM 0
-    for(int i = 1; i <= numero; i++){
+    /* 
+    * LOOP PARA INICIALIZAR A PRIORIDADE
+    * DOS PONTOS NA HEAP.
+    */
+    for(int i = 1; i <= num; i++){
         tamHeap++;
-        if(i == 1 || i == numero)
-            distancia = 0.0;
+        if(i == 1 || i == num)
+            prioridade = INF;
         else{
             if(argv[1][1] == 'h') 
-                distancia = altura2_triangulo(i-1, vetor[i-1], i, vetor[i], i+1, vetor[i+1]);
+                prioridade = altura2_triangulo(i-1, vet_pontos[i-1], i, vet_pontos[i], i+1, vet_pontos[i+1]);
             else    
-                distancia = area_triangulo(i-1, vetor[i-1], i, vetor[i], i+1, vetor[i+1]);
+                prioridade = area_triangulo(i-1, vetor[i-1], i, vetor[i], i+1, vetor[i+1]);
         }
-        vetorheap[tamHeap] = distancia;
-        //inserir arvore(i); isso eu ax q funciona pq o primeiro ponto a ser adicionado eh o vetor[1] ent o i = 1 eh coerente
-        //sobre a ARVORE: eu ax q ela vai precisar apontar para o endereço no vetor normal E no vetor HEAP
-        //pq se ela nn apontar pro vetor HEAP nn tem como saber daonde o elemento removido veio, se eh do ponto 4, ponto 8 etc
-        //mas se ela nn apontar para o vetor normal, na hr de imprimir, q tem q imprimir (4, 9.0) nn vai funcionar, pq tem q imprimir o PONTO e nn a DISTANCIA
+        heap[tamHeap].prio = prioridade;
     }
-    heapfy(tamHeap, vetorheap);
-    for(int i = 1; i <= tamHeap; i++)
-        printf("%f\n", vetorheap[i]);
-    //calcular as distancias e colocar no heap e na arvore
-    //remover com base no argv[]
-    //imprimir
+
+    /* 
+    * LOOP DE REMOCAO DA CABECA DO HEAP
+    * E EVENTUAIS ATUALIZACOES.
+    */
+    while (1){
+        heapfy(tamHeap, heap);
+        atualiza_ts(ts, num);
+        if (heap[1].prio < limite){
+            atualiza_campo_viz(heap, ts, num, 1); // atualiza o campo viz dos vizinhos do ponto heap[1]
+            atualiza_prio_viz(heap, ts, num, 1); // atualiza a prioridade dos vizinhos de ponto heap[1]
+            remove_heap(1); // remove a cabeca do heap
+            ts[heap[1].index] = -1 // atualiza ts para o ponto removido
+            tamHeap--; // atualiza para a impressao
+        }
+        else 
+            break;
+    }
+
+    /* IMPRIME A SAIDA */
+    printf("%d\n", tamHeap);
+    for(int i = 1; i <= tamHeap; i++){
+        if (ts[i] != -1){
+            printf("%f.1 ", i);
+            printf("%f.1\n", vet_pontos[i]);
+        }
+    }
 }
