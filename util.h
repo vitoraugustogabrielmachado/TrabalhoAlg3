ed6bf168e70b6283db2a1d8f0390f540a4a4381d
HugoVictorTs <134705839+HugoVictorTs@users.noreply.github.com>
2026-06-19 17:52:06 -0300

    Update util.h
    coloquei as funções locais da main no util

--- util.h
+++ util.h
@@ -8,4 +8,8 @@ float altura2_triangulo(float x1, float y1, float x2, float y2, float x3, float 
 
 double *inicializaVetor(int numero);
 
+void atualiza_campo_viz(ponto heap[], int ts[]);
+
+void atualiza_prio_viz(ponto heap[], int ts[], float vet_pontos[], int tam, int viz_esq, int viz_dir, char *argv[]);
+
 #endif
