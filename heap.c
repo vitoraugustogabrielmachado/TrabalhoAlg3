dd092a3821c725e0566117aa500277d40a134876
Vitor <vitoraugustogm1@gmail.com>
2026-06-18 21:06:25 -0300

    terminado(?)

--- heap.c
+++ heap.c
@@ -2,33 +2,43 @@
 #include "heap.h"
 
 void troca(ponto heap[], int ts[], int i, int j){
-	ponto aux = heap[i];
-	heap[i] = heap[j];
-	heap[j] = aux;
-
+    ponto aux = heap[i];
+    heap[i] = heap[j];
+    heap[j] = aux;
     ts[heap[i].index] = i;
     ts[heap[j].index] = j;
 }
 
 void sacodeHeap(ponto heap[], int ts[], int pos, int tam){
-    int i = 2*pos;
+    int i = 2 * pos;
     while (i <= tam){
         if (i < tam && heap[i].prio > heap[i + 1].prio)
             i++;
-
         if (heap[i/2].prio <= heap[i].prio)
             break;
-
         troca(heap, ts, i/2, i);
-            
         i *= 2;
     }
 }
 
+/* Sobe um elemento na heap (necessário após diminuir prioridade) */
+void sobeHeap(ponto heap[], int ts[], int pos){
+    int i = pos;
+    while (i > 1 && heap[i/2].prio > heap[i].prio){
+        troca(heap, ts, i/2, i);
+        i /= 2;
+    }
+}
+
+/* Reposiciona um elemento: sobe ou desce conforme necessário */
+void atualiza_pos(ponto heap[], int ts[], int pos, int tam){
+    sobeHeap(heap, ts, pos);
+    sacodeHeap(heap, ts, ts[heap[pos].index], tam);
+}
 
 void heap_remove(ponto heap[], int ts[], int *tam){
-	troca(heap, ts, 1, (*tam)); 
-	sacodeHeap(heap, ts, 1, (*tam) - 1);
+    troca(heap, ts, 1, (*tam));
+    sacodeHeap(heap, ts, 1, (*tam) - 1);
     (*tam)--;
 }
 
@@ -40,15 +50,14 @@ void heap_insere(ponto heap[], int ts[], int n){
     }
 }
 
-void heapfy(ponto heap[], int ts[], int n){ 
-    for (int i = n/2; i >=1; i--)
+void heapfy(ponto heap[], int ts[], int n){
+    for (int i = n/2; i >= 1; i--)
         sacodeHeap(heap, ts, i, n);
 }
 
 int heap_checa(ponto heap[], int tam){
     for (int i = tam; i > 1; i--)
         if (heap[i].prio < heap[i/2].prio)
-            return(0);
-
-    return(1);
+            return 0;
+    return 1;
 }
--- inicio.c
+++ inicio.c
@@ -6,47 +6,71 @@
 #include "util.h"
 #define INF __INT_MAX__
 
-void atualiza_campo_viz(ponto heap[], int ts[], int num){
-    int idx = ts[heap[1].viz_esq];
-    heap[idx].viz_dir = heap[1].viz_dir;
-    idx = ts[heap[1].viz_dir];
-    heap[idx].viz_esq = heap[1].viz_esq;
+void atualiza_campo_viz(ponto heap[], int ts[]){
+    int viz_esq = heap[1].viz_esq;
+    int viz_dir = heap[1].viz_dir;
+
+    if (viz_esq != -1){
+        int idx_esq = ts[viz_esq];
+        heap[idx_esq].viz_dir = viz_dir; 
+    }
+
+    if (viz_dir != -1){
+        int idx_dir = ts[viz_dir];
+        heap[idx_dir].viz_esq = viz_esq; 
+    }
 }
 
-void atualiza_prio_viz(ponto heap[], int ts[], float vet_pontos[], int num, char *argv[]){
-    int idx = ts[heap[1].viz_esq];
-    int idx_esq = ts[heap[idx].viz_esq];
-    int idx_dir = ts[heap[idx].viz_dir];
-    if(heap[idx].viz_esq != -1 && heap[idx].viz_dir != -1) { 
-        if(argv[1][1] == 'h'){
-            heap[idx].prio = altura2_triangulo(heap[idx_esq].index, vet_pontos[heap[idx_esq].index], heap[idx].index, vet_pontos[heap[idx].index], heap[idx_dir].index, vet_pontos[heap[idx_dir].index]);
-            idx = ts[heap[1].viz_dir];
-            idx_esq = ts[heap[idx].viz_esq];
-            idx_dir = ts[heap[idx].viz_dir];
-            heap[idx].prio = altura2_triangulo(heap[idx_esq].index, vet_pontos[heap[idx_esq].index], heap[idx].index, vet_pontos[heap[idx].index], heap[idx_dir].index, vet_pontos[heap[idx_dir].index]);
-        }else{
-            heap[idx].prio = area_triangulo(heap[idx_esq].index, vet_pontos[heap[idx_esq].index], heap[idx].index, vet_pontos[heap[idx].index], heap[idx_dir].index, vet_pontos[heap[idx_dir].index]);
-            idx = ts[heap[1].viz_dir];
-            idx_esq = ts[heap[idx].viz_esq];
-            idx_dir = ts[heap[idx].viz_dir];
-            heap[idx].prio = area_triangulo(heap[idx_esq].index, vet_pontos[heap[idx_esq].index], heap[idx].index, vet_pontos[heap[idx].index], heap[idx_dir].index, vet_pontos[heap[idx_dir].index]);
+void atualiza_prio_viz(ponto heap[], int ts[], float vet_pontos[], int tam, int viz_esq, int viz_dir, char *argv[]){
+
+    if (viz_esq != -1){
+        int idx = ts[viz_esq];
+        int novo_esq = heap[idx].viz_esq;
+        int novo_dir = heap[idx].viz_dir;
+
+        if (novo_esq != -1 && novo_dir != -1){
+            int idx_esq = ts[novo_esq];
+            int idx_dir = ts[novo_dir];
+            if (argv[1][1] == 'h')
+                heap[idx].prio = altura2_triangulo(heap[idx_esq].index, vet_pontos[heap[idx_esq].index], heap[idx].index, vet_pontos[heap[idx].index], heap[idx_dir].index, vet_pontos[heap[idx_dir].index]);
+            else
+                heap[idx].prio = area_triangulo(heap[idx_esq].index, vet_pontos[heap[idx_esq].index], heap[idx].index, vet_pontos[heap[idx].index],heap[idx_dir].index, vet_pontos[heap[idx_dir].index]);
+
+            heapfy(heap, ts, tam);
+        }
+    }
+
+    if (viz_dir != -1){
+        int idx = ts[viz_dir];
+        int novo_esq = heap[idx].viz_esq; 
+        int novo_dir = heap[idx].viz_dir;
+
+        if (novo_esq != -1 && novo_dir != -1){
+            int idx_esq = ts[novo_esq];
+            int idx_dir = ts[novo_dir];
+            if (argv[1][1] == 'h')
+                heap[idx].prio = altura2_triangulo(heap[idx_esq].index, vet_pontos[heap[idx_esq].index], heap[idx].index, vet_pontos[heap[idx].index], heap[idx_dir].index, vet_pontos[heap[idx_dir].index]);
+            else
+                heap[idx].prio = area_triangulo(heap[idx_esq].index, vet_pontos[heap[idx_esq].index], heap[idx].index, vet_pontos[heap[idx].index],heap[idx_dir].index, vet_pontos[heap[idx_dir].index]);
+
+            heapfy(heap, ts, tam);
         }
     }
 }
 
 int main(int argc, char *argv[]){
-    if((argv[1][1] != 'h' && argv[1][1] != 'a') || (argc != 3)){
-        perror("ta errado ai amigao\n"); //quero q saia no terminal e nn q va para o arquivo de saida
-        return(0);
+    if (argc != 3 || (argv[1][1] != 'h' && argv[1][1] != 'a')){
+        fprintf(stderr, "ta errado ai amigao\n");
+        return 0;
     }
-    
-    float limite = atof(argv[2]); // limite aceitavel para a prioridade 
-    int num;
-    scanf("%d", &num); //numero de elementos
 
-    float vet_pontos[num+1]; //o vetor com os pontos (comeca no indice 1 e vai ate num)
-    ponto heap[num+1]; //o heap (vetor de structs)
-    int ts[num+1]; // a tabela de simbolos
+    float limite = atof(argv[2]);
+    int num;
+    scanf("%d", &num);
+
+    float vet_pontos[num + 1];
+    ponto heap[num + 1];
+    int ts[num + 1];
 
     for(int i = 1; i <= num ; i++){
         scanf("%f", &vet_pontos[i]); // leitura dos pontos
@@ -67,54 +91,42 @@ int main(int argc, char *argv[]){
         ts[i] = i; // inicia ts
     }
 
-    float prioridade;
     int tamHeap = 0;
-
-    //para todas as funcoes: OS DOIS VETORES COMEÇAM EM 1 E NAO EM 0
-    /* 
-    * LOOP PARA INICIALIZAR A PRIORIDADE
-    * DOS PONTOS NA HEAP.
-    */
-    for(int i = 1; i <= num; i++){
+    for (int i = 1; i <= num; i++){
         tamHeap++;
-        if(i == 1 || i == num)
+        float prioridade;
+        if (i == 1 || i == num)
             prioridade = INF;
-        else{
-            if(argv[1][1] == 'h') 
-                prioridade = altura2_triangulo(i-1, vet_pontos[i-1], i, vet_pontos[i], i+1, vet_pontos[i+1]);
-            else    
-                prioridade = area_triangulo(i-1, vet_pontos[i-1], i, vet_pontos[i], i+1, vet_pontos[i+1]);
-        }
+        else if (argv[1][1] == 'h')
+            prioridade = altura2_triangulo(i-1, vet_pontos[i-1], i, vet_pontos[i], i+1, vet_pontos[i+1]);
+        else
+            prioridade = area_triangulo(i-1, vet_pontos[i-1], i, vet_pontos[i], i+1, vet_pontos[i+1]);
         heap[tamHeap].prio = prioridade;
     }
-    
-    /* 
-    * LOOP DE REMOCAO DA CABECA DO HEAP
-    * E EVENTUAIS ATUALIZACOES.
-    */
+
     heapfy(heap, ts, tamHeap);
+
     while (1){
         if (heap[1].prio >= limite)
             break;
 
-        atualiza_campo_viz(heap, ts, num); // atualiza o campo viz dos vizinhos do ponto heap[1]
-        atualiza_prio_viz(heap, ts, vet_pontos, num, argv); // atualiza a prioridade dos vizinhos de ponto heap[1]
+        int viz_esq = heap[1].viz_esq;
+        int viz_dir = heap[1].viz_dir;
+
+        atualiza_campo_viz(heap, ts);
+        atualiza_prio_viz(heap, ts, vet_pontos, tamHeap, viz_esq, viz_dir, argv);
 
         int idx = heap[1].index;
-        heap_remove(heap, ts, &tamHeap); // remove a cabeca do heap
-        ts[idx] = -1; // atualiza ts para o ponto removido
+        heap_remove(heap, ts, &tamHeap);
+        ts[idx] = -1;
     }
 
-    /* IMPRIME A SAIDA */
-    
-    for(int i = 1; i <= num; i++){
-        // achei mais facil varrer a ts ao inves do heap
-        // porque assim da para imprimir os pontos na ordem 
-        // outro jeito seria varrer o heap, mas sairia fora de ordem
-        
+    for (int i = 1; i <= num; i++){
         if (ts[i] != -1){
             printf("%d ", i);
             printf("%.1f\n", vet_pontos[i]);
         }
     }
+
+    return 0;
 }
