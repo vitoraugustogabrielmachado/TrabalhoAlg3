9cc6bfa5ef947f6c6e48e7971215bfd4ae407623
HugoVictorTs <134705839+HugoVictorTs@users.noreply.github.com>
2026-06-19 18:00:45 -0300

    Update util.h

--- util.h
+++ util.h
@@ -2,6 +2,8 @@
 #ifndef UTIL
 #define UTIL
 
+#include "heap.h"
+
 float area_triangulo(float x1, float y1, float x2, float y2, float x3, float y3);
 
 float altura2_triangulo(float x1, float y1, float x2, float y2, float x3, float y3);
