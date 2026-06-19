0ec5f1b8ff95e3f1ae3a8a9c538a862e507efb31
Vitor <vitoraugustogm1@gmail.com>
2026-06-18 21:18:48 -0300

    terminado_v2(?)

--- inicio.c
+++ inicio.c
@@ -9,13 +9,11 @@
 void atualiza_campo_viz(ponto heap[], int ts[]){
     int viz_esq = heap[1].viz_esq;
     int viz_dir = heap[1].viz_dir;
-
-    if (viz_esq != -1){
+    if (viz_esq != -1 && ts[viz_esq] != -1){
         int idx_esq = ts[viz_esq];
         heap[idx_esq].viz_dir = viz_dir; 
     }
-
-    if (viz_dir != -1){
+    if (viz_dir != -1 && ts[viz_dir] != -1){
         int idx_dir = ts[viz_dir];
         heap[idx_dir].viz_esq = viz_esq; 
     }
@@ -23,13 +21,12 @@ void atualiza_campo_viz(ponto heap[], int ts[]){
 
 void atualiza_prio_viz(ponto heap[], int ts[], float vet_pontos[], int tam, int viz_esq, int viz_dir, char *argv[]){
 
-    if (viz_esq != -1){
-        int idx = ts[viz_esq];
-        int novo_esq = heap[idx].viz_esq;
-        int novo_dir = heap[idx].viz_dir;
-
-        if (novo_esq != -1 && novo_dir != -1){
-            int idx_esq = ts[novo_esq];
+if (viz_esq != -1 && ts[viz_esq] != -1){
+    int idx = ts[viz_esq];
+    int novo_esq = heap[idx].viz_esq;
+    int novo_dir = heap[idx].viz_dir;
+    if (novo_esq != -1 && novo_dir != -1 && ts[novo_esq] != -1 && ts[novo_dir] != -1){
+        int idx_esq = ts[novo_esq];
             int idx_dir = ts[novo_dir];
             if (argv[1][1] == 'h')
                 heap[idx].prio = altura2_triangulo(heap[idx_esq].index, vet_pontos[heap[idx_esq].index], heap[idx].index, vet_pontos[heap[idx].index], heap[idx_dir].index, vet_pontos[heap[idx_dir].index]);
@@ -38,15 +35,14 @@ void atualiza_prio_viz(ponto heap[], int ts[], float vet_pontos[], int tam, int 
 
             heapfy(heap, ts, tam);
         }
-    }
-
-    if (viz_dir != -1){
-        int idx = ts[viz_dir];
-        int novo_esq = heap[idx].viz_esq; 
-        int novo_dir = heap[idx].viz_dir;
-
-        if (novo_esq != -1 && novo_dir != -1){
-            int idx_esq = ts[novo_esq];
+}
+if (viz_dir != -1 && ts[viz_dir] != -1){
+    int idx = ts[viz_dir];
+    int novo_esq = heap[idx].viz_esq;
+    int novo_dir = heap[idx].viz_dir;
+    if (novo_esq != -1 && novo_dir != -1 && ts[novo_esq] != -1 && ts[novo_dir] != -1){
+        // calcula e atualiza prio normalmente
+        int idx_esq = ts[novo_esq];
             int idx_dir = ts[novo_dir];
             if (argv[1][1] == 'h')
                 heap[idx].prio = altura2_triangulo(heap[idx_esq].index, vet_pontos[heap[idx_esq].index], heap[idx].index, vet_pontos[heap[idx].index], heap[idx_dir].index, vet_pontos[heap[idx_dir].index]);
@@ -54,8 +50,8 @@ void atualiza_prio_viz(ponto heap[], int ts[], float vet_pontos[], int tam, int 
                 heap[idx].prio = area_triangulo(heap[idx_esq].index, vet_pontos[heap[idx_esq].index], heap[idx].index, vet_pontos[heap[idx].index],heap[idx_dir].index, vet_pontos[heap[idx_dir].index]);
 
             heapfy(heap, ts, tam);
-        }
     }
+}
 }
 
 int main(int argc, char *argv[]){
