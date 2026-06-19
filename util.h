59aa88cfbeab249a4c2f10af8eee5aec60610cd7
HugoVictorTs <134705839+HugoVictorTs@users.noreply.github.com>
2026-06-19 17:44:22 -0300

    Update util.h

--- util.h
+++ util.h
@@ -1,7 +1,6 @@
+// BIBLIOTECA PARA FUNCOES AUXILIARES DA MAIN
 #ifndef UTIL
 #define UTIL
-
-//talvez esse arquivo seja inutil dps, mas dps ve isso
 
 float area_triangulo(float x1, float y1, float x2, float y2, float x3, float y3);
 
